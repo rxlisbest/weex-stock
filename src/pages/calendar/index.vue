9b169108f62cdaf2a1d12912a26c373e7768ea62
Roy <rxlisbest@gmail.com>
2019-01-30 16:43:17 +0800

    weex-ui

--- src/pages/calendar/index.vue
+++ src/pages/calendar/index.vue
@@ -1,15 +1,34 @@
 <template>
-  <text>HelloWorld</text>
+  <div>
+    <wxc-button text="Open Popup"
+                @wxcButtonClicked="buttonClicked">
+    </wxc-button>
+    <wxc-popup width="500"
+               pos="left"
+               :show="isShow"
+               @wxcPopupOverlayClicked="overlayClicked">
+    </wxc-popup>
+  </div>
 </template>
 
 <script>
+import { WxcButton, WxcPopup } from 'weex-ui'
 export default {
   name: 'calendar-index',
   created () {
     console.log(2)
   },
-  data () {
-    return {}
+  components: { WxcButton, WxcPopup },
+  data: () => ({
+    isShow: false
+  }),
+  methods: {
+    buttonClicked () {
+      this.isShow = true;
+    },
+    overlayClicked () {
+      this.isShow = false;
+    }
   }
 }
 </script>

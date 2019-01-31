e6d09e599a7823141b4397286817fb9e74869974
Roy <rxlisbest@gmail.com>
2019-01-31 16:11:40 +0800

    增加导航

--- src/pages/calendar/index.vue
+++ src/pages/calendar/index.vue
@@ -1,34 +1,53 @@
 <template>
-  <div>
-    <wxc-button text="Open Popup"
-                @wxcButtonClicked="buttonClicked">
-    </wxc-button>
-    <wxc-popup width="500"
-               pos="left"
-               :show="isShow"
-               @wxcPopupOverlayClicked="overlayClicked">
-    </wxc-popup>
-  </div>
+  <wxc-tab-bar :tab-titles="tabTitles"
+               :tab-styles="tabStyles"
+               title-type="icon"
+               @wxcTabBarCurrentTabSelected="wxcTabBarCurrentTabSelected">
+    <!-- 第一个页面内容-->
+    <div class="item-container" :style="contentStyle"><text>首页</text></div>
+
+    <!-- 第二个页面内容-->
+    <div class="item-container" :style="contentStyle"><text>特别推荐</text></div>
+
+    <!-- 第三个页面内容-->
+    <div class="item-container" :style="contentStyle"><text>消息中心</text></div>
+
+    <!-- 第四个页面内容-->
+    <div class="item-container" :style="contentStyle"><text>我的主页</text></div>
+  </wxc-tab-bar>
 </template>
 
 <script>
-import { WxcButton, WxcPopup } from 'weex-ui'
+import { WxcTabBar, Utils } from 'weex-ui'
+import Config from '../../config/bar.js'
+console.log(Config)
 export default {
-  name: 'calendar-index',
-  created () {
-    console.log(2)
-  },
-  components: { WxcButton, WxcPopup },
+  components: { WxcTabBar },
   data: () => ({
-    isShow: false
+    tabTitles: Config.tabTitles,
+    tabStyles: Config.tabStyles
   }),
+  created () {
+    const tabPageHeight = Utils.env.getPageHeight()
+    // 如果页面没有导航栏，可以用下面这个计算高度的方法
+    // const tabPageHeight = env.deviceHeight / env.deviceWidth * 750
+    const { tabStyles } = this
+    this.contentStyle = { height: (tabPageHeight - tabStyles.height) + 'px' }
+  },
   methods: {
-    buttonClicked () {
-      this.isShow = true;
-    },
-    overlayClicked () {
-      this.isShow = false;
+    wxcTabBarCurrentTabSelected (e) {
+      // const index = e.page
+      // console.log(index)
     }
   }
 }
 </script>
+
+<style scoped>
+  .item-container {
+    width: 750px;
+    background-color: #f2f3f4;
+    align-items: center;
+    justify-content: center;
+  }
+</style>

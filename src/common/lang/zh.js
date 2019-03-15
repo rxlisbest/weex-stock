e7ea80c5c588828c279830988151ea3c7bbcf698
Roy <rxlisbest@gmail.com>
2019-03-15 16:46:38 +0800

    login页面样式

--- src/common/lang/zh.js
+++ src/common/lang/zh.js
@@ -1,5 +1,12 @@
 export const messages = {
-	login: {
-		username: '用户名'
-	}
+  button: {
+    submit: "提交"
+  },
+  login: {
+    title: '登录',
+    form: {
+      username: '用户名',
+      password: '密码'
+    }
+  }
 }
